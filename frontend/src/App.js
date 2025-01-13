bef7e8fc7b4f8a7de9ece9e24236f6d541078535
MedricWorks <142196574+MedricWorks@users.noreply.github.com>
2025-01-13 14:02:57 +0800

    SubscriptionManagement Page
    Added subscription management page with successful API calls and minimal styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Gallery from "./pages/Gallery";
 import SubscriptionForm from "./pages/Subscriptions";
 import SubscriptionChoicePage from "./pages/SubscriptionChoicePage";
 import SubscriptionSuccessPage from "./pages/SubscriptionSuccessPage";
+import SubscriptionManagement from "./pages/SubscriptionManagement"
 
 import Navbar from "./components/Navigation"
 import Footer from "./components/Footer"
@@ -18,6 +19,7 @@ const App = () => {
                 <Route path="/subscriptions" element={<SubscriptionForm />} />
                 <Route path="/subscription-choices" element={<SubscriptionChoicePage />} />
                 <Route path="/subscription-success" element={<SubscriptionSuccessPage />} />
+                <Route path="/subscription-management" element={<SubscriptionManagement />} />
             </Routes>
             <Footer />
         </Router>
